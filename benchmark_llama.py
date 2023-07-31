4cb347fb555844517562c3f03de5dbb5f5f82962
Kye <kye@apacmediasolutions.com>
2023-07-31 18:21:04 -0400

    torch add ng

--- benchmark_llama.py
+++ benchmark_llama.py
@@ -1,7 +1,7 @@
+import time
 import torch
-import math
 import pytest
-from flashtriton.lama import ModelArgs, Attention
+from model import ModelArgs, Attention
 
 # Model Arguments
 args = ModelArgs(dim=512, n_heads=8, n_kv_heads=4, max_batch_size=64, max_seq_len=1024)
@@ -16,17 +16,34 @@ start_pos = 0
 freqs_cis = torch.randn(args.max_batch_size, args.max_seq_len, args.dim).cuda()
 mask = torch.randn(args.max_batch_size, args.max_seq_len, args.dim).cuda()
 
-@pytest.mark.benchmark(group="Attention", timer=torch.cuda.synchronize)
-def test_attention_forward(benchmark):
-    # Run benchmark
-    result = benchmark(model.forward, x, start_pos, freqs_cis, mask)
+def test_attention_forward():
+    # Start timing
+    start_time = time.time()
 
-@pytest.mark.benchmark(group="Attention", timer=torch.cuda.synchronize)
-def test_attention_forward_scaling(benchmark):
+    # Run method
+    model.forward(x, start_pos, freqs_cis, mask)
+
+    # End timing
+    end_time = time.time()
+
+    # Print execution time
+    print(f'Execution time for sequence length 1024: {end_time - start_time} milliseconds')
+
+def test_attention_forward_scaling():
     # Modify sequence length and run benchmark
     args.max_seq_len = 16000
     x = torch.randn(args.max_batch_size, args.max_seq_len, args.dim).cuda()
     freqs_cis = torch.randn(args.max_batch_size, args.max_seq_len, args.dim).cuda()
     mask = torch.randn(args.max_batch_size, args.max_seq_len, args.dim).cuda()
 
-    result = benchmark(model.forward, x, start_pos, freqs_cis, mask)
+    # Start timing
+    start_time = time.time()
+
+    # Run method
+    model.forward(x, start_pos, freqs_cis, mask)
+
+    # End timing
+    end_time = time.time()
+
+    # Print execution time
+    print(f'Execution time for sequence length 16000: {end_time - start_time} milliseconds')
